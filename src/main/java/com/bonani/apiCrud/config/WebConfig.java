81de50f602719628b340733ff37be0b56fb692c7
RenanBonani <bonani.silva@gmail.com>
2022-06-21 07:06:41 -0300

    Correções

--- src/main/java/com/bonani/apiCrud/config/WebConfig.java
+++ src/main/java/com/bonani/apiCrud/config/WebConfig.java
@@ -1,22 +1,32 @@
 package com.bonani.apiCrud.config;
 
+import com.bonani.apiCrud.serialization.converter.YamlJackson2HttpMessageConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+
+    private static final MediaType MEDIA_TYPE_YAML = MediaType.valueOf("application/x-yaml");
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters){
+        converters.add(new YamlJackson2HttpMessageConverter());
+    }
+
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer){
         configurer.favorParameter(true)
-                .parameterName("mediaType")
                 .ignoreAcceptHeader(false)
                 .useRegisteredExtensionsOnly(false)
                 .defaultContentType(MediaType.APPLICATION_JSON)
                 .mediaType("xml", MediaType.APPLICATION_XML)
-                .mediaType("json", MediaType.APPLICATION_JSON);
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("x-yaml", MEDIA_TYPE_YAML);
     }
 }
